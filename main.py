7d551c1bb782602a6194c19c4d2f9c09abd95b48
GodKelvin <kelvin.lehrback@gmail.com>
2022-06-09 18:40:53 -0300

    gerando populacao inicial

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
 import random
 
+from scipy import rand
+
 #Funcao a ser minimizada
 def fitness_function(x):
     return math.cos(x) * (x + 2)
@@ -43,6 +45,23 @@ def decodificacao(bin_number, min, max, size_bin):
         return max
     return x
 
+
+def bin_to_str(array):
+    bin_str = ""
+    for i in range(len(array)):
+        bin_str += str(array[i])
+    return bin_str
+
+def populacao_inicial(tam_pop, tam_bit):
+    populacao = []
+    for _ in range(tam_pop):
+        new_cromossomo = []
+        for _ in range(tam_bit):
+            new_cromossomo.append(random.randint(0,1))
+        populacao.append(new_cromossomo)
+    
+    return populacao
+
 def genetico_binario(tam_populacao_inicial, n_geracoes):
     populacao_inicial = []
     for i in range(tam_populacao_inicial):
@@ -60,7 +79,10 @@ def genetico_binario(tam_populacao_inicial, n_geracoes):
 def main():
     #Para fins de teste
     #print(dec_to_bin(2288967, 22))
-    print(decodificacao('1000101110110101000111', -1, 2, 22))
+    #print(decodificacao('1000101110110101000111', -1, 2, 22))
+    lista = populacao_inicial(10, 3)
+    print(lista)
+    print(bin_to_str(lista[0]))
 
 
 
