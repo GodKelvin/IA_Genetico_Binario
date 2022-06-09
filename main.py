8c9772c6151fcb197cc1693c4eff4d3ef7592c37
GodKelvin <kelvin.lehrback@gmail.com>
2022-06-09 08:10:24 -0300

    implementando conversao de decimanl para binario

--- main.py
+++ main.py
@@ -17,8 +17,16 @@ def get_best(lista, tam_lista):
         return lista[pos1]
     return lista[pos2]
 
-def crossover(pai, filho):
-    
+
+#Alterar para selecao por roleto ao inves de torneio-----------------------------------------
+def crossover(lista, tam_lista):
+    pai1, pai2 = random.randint(0, tam_lista-1), random.randint(0, tam_lista-1)
+    if(lista[pai1] < lista[pai2]):
+        return lista[pai1]
+    return lista[pai2]
+
+def dec_to_bin(int_dec, size_bin):
+    return f"{int_dec:0{size_bin}b}"
 
 def genetico_binario(tam_populacao_inicial, n_geracoes):
     populacao_inicial = []
@@ -33,11 +41,13 @@ def genetico_binario(tam_populacao_inicial, n_geracoes):
         
         print(populacao_inicial)
         print(new_population)
-        input()
 
 
 def main():
-    genetico_binario(10, 10)
+    #genetico_binario(10, 10)
+    print("{0:022b}".format(2288967))
+    print("{0:022b}".format(2288967))
+    print(dec_to_bin(2288967,22))
 
 
 
